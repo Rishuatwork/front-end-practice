687c48aa9f5d2351c3ad7db316c643ed24a88ea4
Rishabh Sharma <rishabhsharma.business148@gmail.com>
2026-03-27 13:26:32 +0530

    Cleaned JS for Quiz App

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -97,8 +97,8 @@ function selectAnswer(e) {
 function showScore() {
   resetState();
   questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-  nextButton.innerHTML = "Play Again"
-  nextButton.style.display = "block"
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
 }
 
 function handleNextButton() {
